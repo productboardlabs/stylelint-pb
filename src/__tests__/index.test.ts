d62883c835939ee4af78dc14b566e7ef34292f6c
Jakub Benes <jukben@gmail.com>
2019-03-30 18:34:20 +0100

    First try

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,40 @@
+import * as testRule from "stylelint-test-rule-tape";
+import rule from "../index";
+const { messages, ruleName } = rule;
+
+testRule(rule, {
+  ruleName,
+  config: [
+    {
+      snowWhite: "#f4f5e2"
+    }
+  ],
+  fix: true,
+
+  accept: [
+    {
+      code: "a { color: @snowWhite; }"
+    },
+    {
+      code: "a { font-size: 3rem; }"
+    }
+  ],
+
+  reject: [
+    {
+      code: "a { color: #f4f5e2; }",
+      fixed: "a { color: @snowWhite; }",
+
+      message: "You are a bad variable user",
+      line: 1,
+      column: 12
+    },
+    {
+      code: "a { color: #000F }",
+
+      message: "You are a bad variable user",
+      line: 1,
+      column: 16
+    }
+  ]
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hey!");
+import stylelint from "stylelint";
+
+export const ruleName = "color-hex-case";
+
+export const messages = stylelint.utils.ruleMessages(ruleName, {
+  expected: "Error"
+});
+
+const rule = function(expectation, options, context) {
+  return (root, result) => {};
+};
+
+rule.ruleName = ruleName;
+rule.messages = messages;
+
+export default rule;
